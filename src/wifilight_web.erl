9d17b777d6f9834afdfdfd873e4d3b847353e74c
rakvat <rakvat@gmx.net>
2014-09-16 00:39:23 +0200

    first color request experiments

--- src/wifilight_web.erl
+++ src/wifilight_web.erl
@@ -21,11 +21,47 @@ stop() ->
     mochiweb_http:stop(?MODULE).
 
 loop(Req, DocRoot) ->
+    Port = 5577,
+    Ip = {192,168,1,23},
+
     "/" ++ Path = Req:get(path),
     try
         case Req:get(method) of
             Method when Method =:= 'GET'; Method =:= 'HEAD' ->
                 case Path of
+                    "red" ->
+                        {ok, Socket} = gen_tcp:connect(Ip, Port, [binary, {active,true}]),
+                        SetColor = [16#56, 255, 0, 0, 16#AA],
+                        gen_tcp:send(Socket, SetColor),
+                        gen_tcp:close(Socket),
+                        Req:respond({200, [{"Content-Type", "text/plain"}], "ok"});
+                    "green" ->
+                        {ok, Socket} = gen_tcp:connect(Ip, Port, [binary, {active,true}]),
+                        SetColor = [16#56, 0, 255, 0, 16#AA],
+                        gen_tcp:send(Socket, SetColor),
+                        gen_tcp:close(Socket),
+                        Req:respond({200, [{"Content-Type", "text/plain"}], "ok"});
+                    "blue" ->
+                        {ok, Socket} = gen_tcp:connect(Ip, Port, [binary, {active,true}]),
+                        SetColor = [16#56, 0, 0, 255, 16#AA],
+                        gen_tcp:send(Socket, SetColor),
+                        gen_tcp:close(Socket),
+                        Req:respond({200, [{"Content-Type", "text/plain"}], "ok"});
+                    "color" ->
+                        {ok, Socket} = gen_tcp:connect(Ip, Port, [binary, {active,true}]),
+                        QueryStringData = Req:parse_qs(),
+                        Red = list_to_integer(proplists:get_value("r", QueryStringData, "0")),
+                        Green = list_to_integer(proplists:get_value("g", QueryStringData, "0")),
+                        Blue = list_to_integer(proplists:get_value("b", QueryStringData, "0")),
+                        SetColor = [16#56, Red, Green, Blue, 16#AA],
+                        gen_tcp:send(Socket, SetColor),
+                        gen_tcp:close(Socket),
+                        Req:respond({200, [{"Content-Type", "text/plain"}], "ok"});
+                    "hello" ->
+                        QueryStringData = Req:parse_qs(),
+                        Username = proplists:get_value("username", QueryStringData, "Anonymous"),
+                        Req:respond({200, [{"Content-Type", "text/plain"}],
+                                            "Hello " ++ Username ++ "!\n"});
                     _ ->
                         Req:serve_file(Path, DocRoot)
                 end;
